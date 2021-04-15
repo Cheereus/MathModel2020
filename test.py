2588cda3671d3be74e5013d4d077026ca3a215ef
Cheereus <fanwei1995@hotmail.com>
2021-04-15 12:34:09 +0800

    updates

--- test.py
+++ test.py
@@ -5,17 +5,17 @@ import matplotlib.pyplot as plt
 from Filter import cb_filter
 
 # 获取一位受试者的训练数据
-train_data = np.array(joblib.load('data/event_data_by_S.pkl')[0])
+train_data = np.array(joblib.load('data/train_data_by_S.pkl')[0])
 
-print(train_data.shape)
+print(np.array(train_data[0]).T[0])
 
-x = range(0, 800, 4)
-y = np.array(train_data[0]).T[0]
-
-plt.plot(x, y, linestyle='-', label='P300', c='k')
-y = np.array(train_data[6]).T[0]
-# plt.plot(x, y, linestyle='--', label='non P300', c='k')
-my_x_ticks = np.arange(0, 840, 100)
+x = range(0, 3125)
+y = cb_filter(np.array(train_data[0]).T[0])
+plt.plot(x, y, linestyle='-', label='after', c='k')
+# y = np.array(train_data[6]).T[0]
+# y = np.array(train_data[0]).T[0]
+# plt.plot(x, y, linestyle='-', label='before', c='k')
+my_x_ticks = np.arange(0, 3500, 100)
 plt.xticks(my_x_ticks)
 # plt.legend(loc='best')
 plt.grid()
